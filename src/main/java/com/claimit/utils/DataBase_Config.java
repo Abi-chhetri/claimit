c7f766b264192b0214c57cb2556cfbc0c8d12018
xettriabi6 <abichhetri75@gmail.com>
2026-04-09 19:35:56 +0545

    Re-structured the project

--- src/main/java/com/claimit/utils/DataBase_Config.java
+++ src/main/java/com/claimit/utils/DataBase_Config.java
@@ -10,7 +10,7 @@ public class DataBase_Config {
 	private static String dbName = "claim_it";
 	private static String dbUrl = "jdbc:mysql://localhost:3306/" + dbName;
 	private static String userName = "claim_it";
-	private static String password = "claim@890";
+	private static String password = "claimit@890";
 
 	public static Connection getConection() throws SQLException, ClassNotFoundException {
 		Class.forName("com.mysql.cj.jdbc.Driver");
